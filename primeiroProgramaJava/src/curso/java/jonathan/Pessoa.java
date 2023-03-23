75a1088826ecd650af7c132998ef28f3e25117f8
jonathanppmaia <jonathanppmaia@gmail.com>
2023-03-23 01:52:13 -0300

    Add age attribute in class

--- primeiroProgramaJava/src/curso/java/jonathan/Pessoa.java
+++ primeiroProgramaJava/src/curso/java/jonathan/Pessoa.java
@@ -1,5 +1,25 @@
 package curso.java.jonathan;
 
 public class Pessoa {
+	
+	private String nome;
+	
+	private String sobrenome;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
 
 }
