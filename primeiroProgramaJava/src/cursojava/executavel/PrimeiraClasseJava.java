f02cee94365320ca87f7ec0737438ce02a6109ab
jonathanppmaia <jonathanppmaia@gmail.com>
2023-03-23 01:14:22 -0300

    Project start

---
+++ primeiroProgramaJava/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	/*Main é um método executavel em java */
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro Programa em Java do Curso Formação Java Web");
+		System.out.println("Java é orientado a objetos");
+	}
+
+}
